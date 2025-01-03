9e1043c5c86a64514f76a2f7c8568ca1f1489392
2019ASP02 <149567036+2019ASP02@users.noreply.github.com>
2025-01-03 21:08:45 +0530

    define the sniffer

--- code.py
+++ code.py
@@ -1,5 +1,20 @@
 import os
 import sys
 import socket
-import struck
+import struct
 
+def sniffer:
+    if os.name == 'nt':
+        sniffer_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+        host = socket.gethostname(socket.gethostname())
+        sniffer_socket.bind((host,0))
+        sniffer_socket.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
+        sniffer_socket.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
+        print(f"Sniffer started on WIndows, host : {host}")
+    elif os.name =='posix':
+        sniffer_socket = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.ntohs(0x0003))
+        print("Sniffer started on Linux/Unix")
+    else:
+        print("Unsupported platform.")
+        sys.exit(1)
+    return sniffer_socket
